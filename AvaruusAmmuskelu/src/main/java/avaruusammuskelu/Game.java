accef15a0f205b8b7582bab43a8c0c3bde592a50
sorja <miro.sorja@gmail.com>
2013-12-17 01:24:09 +0200

    Updated game libraries, working hours

---
+++ AvaruusAmmuskelu/src/main/java/avaruusammuskelu/Game.java
@@ -0,0 +1,18 @@
+package avaruusammuskelu;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+class Game {
+
+    public Game() {
+    }
+
+    public void start() {
+        System.out.println("Wow, it worked");
+    }
+
+}
